cdecba95b52270e43b996f2b8057b762defa2797
vedantkesharia <keshariavedant@gmail.com>
2023-08-12 15:24:17 +0530

    commit new9

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,14 +25,13 @@ const Feed = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
+  useEffect(() => {
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
     const data = await response.json();
 
     setAllPosts(data);
   };
-
-  useEffect(() => {
     fetchPosts();
   }, []);
 

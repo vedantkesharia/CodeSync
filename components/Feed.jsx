9359a62eb187ec5be90163e49d1c6e4727e6fd3e
vedantkesharia <keshariavedant@gmail.com>
2023-08-12 15:48:13 +0530

    commit11 new

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -27,7 +27,9 @@ const Feed = () => {
 
   useEffect(() => {
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
+    const response = await fetch("/api/prompt",{
+      next: { revalidate: 10, cache: "no-store" },
+    });
     const data = await response.json();
 
     setAllPosts(data);
